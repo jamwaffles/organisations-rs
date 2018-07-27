f5f3f08c2a2fe8d555ec49379e458c8d619f9494
Peter East <petereast@users.noreply.github.com>
2018-07-27 18:28:40 +0100

    [S] Add some docker environment (#2)
    * C O N T A I N E R I Z E
    
    * U N D O C K E R I S E

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use operations::health;
 
 fn main() {
     server::new(|| App::new().resource("/health", |r| r.method(Method::GET).f(health)))
-        .bind("127.0.0.1:8080")
+        .bind("0.0.0.0:8080")
         .unwrap()
         .run();
 }
