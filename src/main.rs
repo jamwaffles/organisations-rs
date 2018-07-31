2ec650dfb146bb4eedb4227a3e788ae13acce5ae
James Waples <jamwaffles@gmail.com>
2018-07-31 16:33:12 +0100

    Inject JWT into app, not routes

--- src/main.rs
+++ src/main.rs
@@ -55,9 +55,9 @@ fn main() {
     server::new(move || {
         App::with_state(AppState {
             eventstore: addr.clone(),
-        }).resource("/health", |r| r.get().f(health))
+        }).middleware(middleware::InjectJwt)
+        .resource("/health", |r| r.get().f(health))
         .resource("/get-organisation-members/{organisation_id}", |r| {
-            r.middleware(middleware::InjectJwt);
             r.middleware(enforcement::AdminOnly);
             r.get().with(get_organisation_members);
         })
